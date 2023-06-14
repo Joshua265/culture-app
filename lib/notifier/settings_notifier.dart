7965a4dc12e3ec0ee8f1778f821658fffb9834a9
Joshua265 <joshua_noel@gmx.de>
2023-06-14 17:15:59 +0200

    persistent settings

--- lib/notifier/settings_notifier.dart
+++ lib/notifier/settings_notifier.dart
@@ -1,30 +1,52 @@
 import 'package:culture_app/models/settings.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsNotifier extends StateNotifier<Settings> {
-  SettingsNotifier() : super(Settings());
+  SettingsNotifier() : super(Settings()) {
+    _loadSettingsFromPrefs();
+  }
 
-  void toggleDarkMode() {
+  Future<void> _loadSettingsFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    state = Settings(
+      darkMode: prefs.getBool('darkMode') ?? false,
+      language: prefs.getString('language') ?? 'de',
+      city: prefs.getString('city') ?? 'Heilbronn',
+    );
+  }
+
+  Future<void> _saveSettingsToPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('darkMode', state.darkMode);
+    await prefs.setString('language', state.language);
+    await prefs.setString('city', state.city);
+  }
+
+  void toggleDarkMode() async {
     state = Settings(
       darkMode: !state.darkMode,
       language: state.language,
       city: state.city,
     );
+    await _saveSettingsToPrefs();
   }
 
-  void setLanguage(String language) {
+  void setLanguage(String language) async {
     state = Settings(
       darkMode: state.darkMode,
       language: language,
       city: state.city,
     );
+    await _saveSettingsToPrefs();
   }
 
-  void setCity(String city) {
+  void setCity(String city) async {
     state = Settings(
       darkMode: state.darkMode,
       language: state.language,
       city: city,
     );
+    await _saveSettingsToPrefs();
   }
 }
